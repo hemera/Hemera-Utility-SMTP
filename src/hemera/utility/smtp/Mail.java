f4d68642527a4f9ef63ea6db46ef20d7d552df30
Yi Wang <ywang@looply.me>
2012-11-17 12:07:51 -0500

    Converted transport into singleton service.

--- src/hemera/utility/smtp/Mail.java
+++ src/hemera/utility/smtp/Mail.java
@@ -10,12 +10,12 @@ import javax.mail.internet.MimeMessage;
 
 /**
  * <code>Mail</code> defines implementation of Email
- * that can be sent via <code>SMTPTransport</code>.
+ * that can be sent via <code>SMTPService</code>.
  * It provides various convenient methods for setting
  * the parameters of an Email. It internally depends
- * on the <code>SMTPTransport</code> being connected,
+ * on the <code>SMTPService</code> being connected,
  * thus an instance cannot be constructed until the
- * <code>SMTPTransport</code> is connected.
+ * <code>SMTPService</code> is connected.
  *
  * @author Yi Wang (Neakor)
  * @version 1.0.0
@@ -24,11 +24,9 @@ public class Mail extends MimeMessage {
 
 	/**
 	 * Constructor of <code>Mail</code>.
-	 * @param transport The <code>SMTPTransport</code>
-	 * used to send this message.
 	 */
-	public Mail(final SMTPTransport transport) {
-		super(transport.session);
+	public Mail() {
+		super(SMTPService.instance.getSession());
 	}
 	
 	/**
---
+++ src/hemera/utility/smtp/SMTPService.java
@@ -0,0 +1,154 @@
+package hemera.utility.smtp;
+
+import hemera.core.utility.logging.FileLogger;
+
+import java.nio.charset.Charset;
+import java.util.Properties;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock.ReadLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock.WriteLock;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+
+/**
+ * <code>SMTPService</code> defines the implementation
+ * of a singleton transport service that allows sending
+ * messages via the SMTP protocol.
+ * <p>
+ * <code>SMTPService</code> provides necessary thread
+ * safety measures to ensure that the service can be
+ * accessed in different threads.
+ *
+ * @author Yi Wang (Neakor)
+ * @version 1.0.0
+ */
+public enum SMTPService {
+	/**
+	 * The singleton instance.
+	 */
+	instance;
+	
+	/**
+	 * The <code>FileLogger</code> instance.
+	 */
+	private final FileLogger logger;
+	/**
+	 * The <code>ReadLock</code> of the transport.
+	 */
+	private final ReadLock readLock;
+	/**
+	 * The <code>WriteLock</code> of the transport.
+	 */
+	private final WriteLock writeLock;
+	/**
+	 * The <code>Session</code> instance.
+	 */
+	private Session session;
+	/**
+	 * The <code>Transport</code> instance used to
+	 * send <code>Mail</code>.
+	 */
+	private Transport transport;
+	
+	/**
+	 * Constructor of <code>SMTPService</code>.
+	 */
+	private SMTPService() {
+		this.logger = FileLogger.getLogger(this.getClass());
+		final ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
+		this.readLock = lock.readLock();
+		this.writeLock = lock.writeLock();
+	}
+
+	/**
+	 * Connect the service to the given host.
+	 * @param host The <code>String</code> SMTP host
+	 * address.
+	 * @param port The <code>int</code> host port to
+	 * connect to.
+	 * @param username The <code>String</code> login
+	 * user name.
+	 * @param password The <code>String</code> login
+	 * password.
+	 * @param requireTLS <code>true</code> if login
+	 * requires TLS. <code>false</code> otherwise.
+	 * @throws MessagingException If establishing the
+	 * SMTP connection failed.
+	 */
+	public void connect(final String host, final int port, final String username,
+			final String password, final boolean requireTLS) throws MessagingException {
+		this.writeLock.lock();
+		try {
+			// Disconenct old transport if necessary.
+			if (this.transport != null) {
+				this.disconnect();
+			}
+			// Check JVM encoding.
+			final String encoding = Charset.defaultCharset().displayName();
+			if (!encoding.equals("UTF-8") && !encoding.equals("UTF-16")) {
+				this.logger.warning("JVM encoding is not UTF-8 nor UTF-16. Mail contents may not be encoded correctly.");
+			}
+			// Create session.
+			final Properties properties = new Properties();
+			properties.put("mail.transport.protocol", "smtp");
+			properties.put("mail.smtp.auth", "true");
+			properties.put("mail.smtp.starttls.enable", String.valueOf(requireTLS));
+			this.session = Session.getInstance(properties);
+			// Retrieve and connect transport.
+			this.transport = this.session.getTransport("smtp");
+			this.transport.connect(host, port, username, password);
+		} finally {
+			this.writeLock.unlock();
+		}
+	}
+	
+	/**
+	 * Send the given mail using this SMTP transport.
+	 * @param mail The <code>Mail</code> to be sent.
+	 * @throws MessagingException If sending failed.
+	 */
+	public void sendMail(final Mail mail) throws MessagingException {
+		if (mail == null) return;
+		this.readLock.lock();
+		try {
+			if (this.transport == null || !this.transport.isConnected()) {
+				throw new MessagingException("SMTP service is not yet connected.");
+			}
+			this.transport.sendMessage(mail, mail.getRecipients(Message.RecipientType.TO));
+		} finally {
+			this.readLock.unlock();
+		}
+	}
+	
+	/**
+	 * Disconnect the SMTP transport from its provider.
+	 * @throws MessagingException If closing connection
+	 * failed.
+	 */
+	public void disconnect() throws MessagingException {
+		this.writeLock.lock();
+		try {
+			this.transport.close();
+			this.session = null;
+			this.transport = null;
+		} finally {
+			this.writeLock.unlock();
+		}
+	}
+	
+	/**
+	 * Retrieve the session instance.
+	 * @return The <code>Session</code> instance.
+	 */
+	Session getSession() {
+		this.readLock.lock();
+		try {
+			return this.session;
+		} finally {
+			this.readLock.unlock();
+		}
+	}
+}
